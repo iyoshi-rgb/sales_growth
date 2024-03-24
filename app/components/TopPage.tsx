956ab974ff2ee91f1e1911cd66b5acd0dfbb49e3
yoshito <yoshito.i.2002@gmail.com>
2024-03-24 17:16:49 +0900

    fix icon

--- app/components/TopPage.tsx
+++ app/components/TopPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Image from "next/image";
+import Logo from "@/app/logo.png";
 
 const TopPage = () => {
   return (
     <div className="flex justify-center items-center">
       <div className="text-center">
-        <p className="font-bold text-2xl">Sales Growth</p>
+        <Image src={Logo} alt="" width={300} height={300} className="ml-14" />
         <div className="mt-5">
           <p className="font-serif">
             営業組織における<br></br>
--- app/page.tsx
+++ app/page.tsx
@@ -3,30 +3,25 @@ import { redirect } from "next/navigation";
 import TopPage from "./components/TopPage";
 import Login from "./components/Login";
 
-
 export default async function Index() {
-  
-    const supabase = createClient();
+  const supabase = createClient();
 
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-  
-    if (user) {
-      return redirect("/home");
-    }
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-  
- 
+  if (user) {
+    return redirect("/home");
+  }
+
   return (
     <div className="flex h-screen">
-      <div className="w-1/2 h-full flex justify-center items-center bg-gray-100 ">
-        <TopPage/>
+      <div className="w-1/2 h-full flex justify-center items-center bg-gray-300 ">
+        <TopPage />
       </div>
       <div className="w-1/2 h-full flex justify-center items-center">
-        <Login/>
+        <Login />
       </div>
     </div>
-
   );
-  }
+}
