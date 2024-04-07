cea07690ef510f0152587e8559ec8022ec7bbe7e
yoshito <yoshito.i.2002@gmail.com>
2024-04-07 20:46:18 +0900

    fix Nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -29,10 +29,6 @@ const Nav = async ({ org }: any) => {
               <li className="hover:text-white block px-6 py-2">Data</li>
             </Link>
 
-            <Link href="/sample">
-              <li className="hover:text-white block px-6 py-2">List2</li>
-            </Link>
-
             <MemberDropDown members={members} org={org} />
           </ul>
         </nav>
