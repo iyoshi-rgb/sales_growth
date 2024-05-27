48c6882c8b1c9f19bee0f88aa44c26451fd55e56
yoshito <yoshito.i.2002@gmail.com>
2024-05-27 14:22:34 +0900

    #fix miss commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,58 +1,21 @@
-import * as React from "react";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
-import Logo from "@/app/logo.png";
-import Image from "next/image";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import React from "react";
 import AuthButton from "./AuthButton";
 
-interface HeaderProps {
-  sections: ReadonlyArray<{
-    title: string;
-    url: string;
-  }>;
+interface Props {
   title: string;
 }
 
-export default function Header(props: HeaderProps) {
-  const { sections, title } = props;
-
+const Header = ({ title }: Props) => {
   return (
-    <React.Fragment>
-      <Toolbar sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Image src={Logo} alt="" width={100} height={250} />
-        <Typography
-          component="h2"
-          variant="h5"
-          color="inherit"
-          align="center"
-          noWrap
-          sx={{ flex: 1 }}
-        >
-          {title}
-        </Typography>
-        <IconButton className="">
-          <AccountCircleIcon />
-        </IconButton>
-        <AuthButton />
-      </Toolbar>
-
-      <div className="flex justify-center items-center gap-10">
-        {sections.map((section) => (
-          <Link
-            color="inherit"
-            noWrap
-            key={section.title}
-            variant="body2"
-            href={section.url}
-            sx={{ p: 1, flexShrink: 0 }}
-          >
-            {section.title}
-          </Link>
-        ))}
+    <div>
+      <div className="flex justify-between items-center">
+        <p className="font-bold text-4xl py-4 pl-6">{title}</p>
+        <div className="mr-5">
+          <AuthButton />
+        </div>
       </div>
-    </React.Fragment>
+    </div>
   );
-}
+};
+
+export default Header;
